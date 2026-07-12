251214f50e9b659b5bc72c91f7251fe2c16f54f7
payoshi01 <guptapayoshi2016@gmail.com>
2026-07-12 14:06:50 +0530

    Added React Hands-on 1 - React Environment Setup

---
+++ ReactJS-HandsOn/myfirstreact/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to React</h1>
+      <h2>My First React Application</h2>
+      <p>This is my first React Hands-on.</p>
+    </div>
+  );
+}
+
+export default App;
